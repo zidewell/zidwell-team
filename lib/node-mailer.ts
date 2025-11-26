ba975618d51040a25a8d5cc03717cd4d8472f12f
Abbalolo <verifiedaboki@gmail.com.com>
2025-11-26 18:55:44 +0100

    error

--- lib/node-mailer.ts
+++ lib/node-mailer.ts
@@ -1,9 +1,7 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  host: "smtp.zoho.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
